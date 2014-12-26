7187b278c90d8623ce880ad4c135ac62501d4b27
MidoriYakumo <macrobull.acg.0@gmail.com>
2014-12-27 03:30:37 +0800

    add comparison

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ import os, sys, time, glob
 import h5py
 
 from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC, NuSVC
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.lda import LDA
 from sklearn.qda import QDA
+from sklearn.semi_supervised import LabelPropagation, LabelSpreading
 
 
 
@@ -43,6 +44,7 @@ FONT = os.environ['HOME'] + "/.fonts/Android/DroidSansFallback.ttf"
 disp = True
 #disp = False
 tuning = True
+comparison = True
 
 from PIL.ImageShow import UnixViewer, register
 
@@ -63,24 +65,23 @@ K = 8
 
 def denominant_color(filename, **kwargs):
 
-	kwargsd = dict(show_img = 'colors', cache = 'cache.hdf5', vect_sort = 'L')
+	kwargsd = dict(show_img = 'colors', cache = 'cache.hdf5', cache_mode = 'r', vect_sort = 'L')
 	kwargsd.update(kwargs)
 
+	cRGB = None
+
+	cache_mode = kwargsd.get('cache_mode')
 	cache = kwargsd.get('cache')
 	if cache:
-		md5=os.popen('md5sum '+filename).read().split()[0]
-		if not os.path.isfile(cache):
+		md5=os.popen("md5sum '"+filename+"'").read().split()[0]
+		if os.path.isfile(cache):
+			hdf = h5py.File(cache, "r+")
+			# TODO check version
+			if md5 in hdf:
+				cRGB = np.array(hdf[md5])
+				print('[Cache hit] ' + filename)
+		elif 'w' in cache_mode:
 			hdf = h5py.File(cache, "w")
-			hdf.close()
-
-		# TODO check version
-
-		hdf = h5py.File(cache, "r+")
-		if md5 in hdf:
-			cRGB = np.array(hdf[md5])
-			hdf.close()
-			return cRGB
-
 
 	def putCircle(xy, color, **kwargs):
 		r = CIR_RADIUS
@@ -113,34 +114,38 @@ def denominant_color(filename, **kwargs):
 		painter = ImageDraw.Draw(canvas)
 
 
-	# KMeans clustering
-	clt = KMeans(K, n_jobs = -1,
-#		max_iter = 200, n_init = 8,
-		init = "k-means++")
-	clt.fit(np.array(thumb).reshape((-1,3)))
+	if cRGB is None:
+		# KMeans clustering
+		clt = KMeans(K, n_jobs = -1,
+	#		max_iter = 200, n_init = 8,
+			init = "k-means++")
+		clt.fit(np.array(thumb).reshape((-1,3)))
 
-	# Sort colors into a vector
-	rRank = list(range(K))
-	cRGB = clt.cluster_centers_
-	cHLS = np.array([rgb_to_hls(*c) for c in cRGB])
+		# Sort colors into a vector
+		rRank = list(range(K))
+		cRGB = clt.cluster_centers_
+		cHLS = np.array([rgb_to_hls(*c) for c in cRGB])
 
-	vect_sort = kwargsd.get('vect_sort')
-	if vect_sort == 'H': #	#Sort with Hue
-		rRank = sorted(rRank, key=lambda n:cHLS[n,0])
-	if vect_sort == 'L': # Sort with Luminance
-		rRank = sorted(rRank, key=lambda n:cHLS[n,1])
+		vect_sort = kwargsd.get('vect_sort')
+		if vect_sort == 'H': #	#Sort with Hue
+			rRank = sorted(rRank, key=lambda n:cHLS[n,0])
+		if vect_sort == 'L': # Sort with Luminance
+			rRank = sorted(rRank, key=lambda n:cHLS[n,1])
 
-#	#Make it best cut on Hue
-#	cDL = cHLS[rRank][1:,0] - cHLS[rRank][:-1,0]
-##	print(1 + cHLS[rRank][0,0] - cHLS[rRank][-1,0], cDL)
-#	if cDL.max() > 1 + cHLS[rRank][0,0] - cHLS[rRank][-1,0]:
-#		rRank = rRank[cDL.argmax()+1:] + rRank[:cDL.argmax()+1]
+	#	#Make it best cut on Hue
+	#	cDL = cHLS[rRank][1:,0] - cHLS[rRank][:-1,0]
+	##	print(1 + cHLS[rRank][0,0] - cHLS[rRank][-1,0], cDL)
+	#	if cDL.max() > 1 + cHLS[rRank][0,0] - cHLS[rRank][-1,0]:
+	#		rRank = rRank[cDL.argmax()+1:] + rRank[:cDL.argmax()+1]
 
-	#Pass to output
-	cRGB = cRGB[rRank]
+		#Pass to output
+		cRGB = cRGB[rRank]
+	else:
+		kwargsd['show_img'] = None
 
 	if cache:
-		hdf.create_dataset(md5, data=cRGB)
+		if 'w' in cache_mode and md5 not in hdf:
+			hdf.create_dataset(md5, data=cRGB)
 		hdf.close()
 
 	for i, color in enumerate(cRGB):
@@ -166,10 +171,14 @@ if __name__ == '__main__':
 	cCntr = 0
 	cCnt = {}
 	dsDir = 'img/'
-	for fn in os.listdir(dsDir):
+	fileList = list(os.listdir(dsDir))
+	total = len(fileList)
+	print("[Info] Extracting features from {} files.".format(total))
+	for i, fn in enumerate(fileList):
 #		try:
 			if fn.endswith('jpg') or fn.endswith('png'):
-				X.append(denominant_color(dsDir + fn).reshape(-1))
+#				print('{:4}/{:4}:{}'.format(i, total, fn))
+				X.append(denominant_color(dsDir + fn, cache_mode = 'w').reshape(-1))
 				chars = fn.split('.')[0] # CHARACTER.X.jpg
 				if chars not in c2y:
 					y2c[cCntr] = chars
@@ -181,7 +190,7 @@ if __name__ == '__main__':
 #		except Exception as e:
 #			print(fn, e)
 
-	print("Dataset y={}, count = {}".format(c2y, cCnt))
+	print("[Classifier] Dataset y={}, count = {}".format(c2y, cCnt))
 
 	X = np.array(X)
 	y = np.array(y)
@@ -196,11 +205,11 @@ if __name__ == '__main__':
 
 		sMax = 0
 #		for p in np.linspace(-20.0, 20.0, 15):
-		for p in np.linspace(375., 255.*24, 15):
-#		for p in range(1, 20):
-			param = dict(radius = p)
+#		for p in np.linspace(1e-4, 0.20, 15):
+		for p in range(2, 20):
+			param = dict()
 
-			clf = RadiusNeighborsClassifier(**param)
+			clf = KNeighborsClassifier(**param)
 
 			s = 0
 			for r_c in r_cs:
@@ -209,56 +218,75 @@ if __name__ == '__main__':
 				clf.fit(X_c[:sCnt], y_c[:sCnt])
 				s += clf.score(X_c[sCnt:],y_c[sCnt:])
 
-			print("Cross_val score = {:2},\t param = {}".format(s, param))
+			print("[Classifier] Cross_valid score = {:2},\t param = {}".format(s, param))
 
 			if s>sMax: clfBest, sMax = clf, s
 
 		clf = clfBest
-		print(clf.get_params())
+		print("[Classifier] ", clf.get_params())
 
 	else:
-#		clf = SVC(kernel = 'poly', C = 1., degree = 5, gamma = 0.8, tol = 1.0) # fair
-#		clf = AdaBoostClassifier() # bad
-#		clf = BaggingClassifier(n_estimators = 45) # fair+
-#		clf = GradientBoostingClassifier() # fair+
-#		clf = RandomForestClassifier() # bad
-#		clf = LDA() # good
-#		clf = GaussianNB() # fair
-#		clf = MultinomialNB() # good
-#		clf = BernoulliNB() # suck
-#		clf = KNeighborsClassifier(n_neighbors = 5, weights = 'distance') # fair
-#		clf = RadiusNeighborsClassifier() # suck
-		clf = QDA()
+		classifiers = [
+			SVC(kernel = 'poly', C = 1., degree = 5, gamma = 0.8, tol = 1.0), # fair
+#			NuSVC(kernel = 'linear', nu = p, degree = 5), #suck
+			MultinomialNB(), # fair
+			GaussianNB(), # fair
+#			BernoulliNB(), # suck
+			LDA(), # good
+#			QDA(), # bug
+			AdaBoostClassifier(), # bad
+			GradientBoostingClassifier(), # fair+ slow
+			BaggingClassifier(n_estimators = 45), # fair+
+			DecisionTreeClassifier(), # bad
+#			ExtraTreeClassifier(), # bad
+			RandomForestClassifier(), # bad
+#			RadiusNeighborsClassifier(), # suck eee
+			LabelPropagation(kernel = 'knn'), #suck
+#			LabelSpreading(kernel = 'knn'), #suck
+			KNeighborsClassifier(n_neighbors = 5, weights = 'distance') # fair
+			]
+#		comparison = False
+		if not comparison:
+			classifiers = classifiers[:1]
+		for clf in classifiers:
+			clfName = str(clf).split('(')[0]
+			s = 0
+			for r_c in r_cs:
+				X_c, y_c = X[r_c], y[r_c]
+				sCnt = int(0.9 * len(X))
+				clf.fit(X_c[:sCnt], y_c[:sCnt])
+				s += clf.score(X_c[sCnt:],y_c[sCnt:])
 
+			print("[Classifier] {} cross_valid score:{:3}".format(clfName, s/len(r_cs)))
 
+			clf.fit(X, y)
+			print("[Classifier] {} Training score:{}".format(clfName, clf.score(X,y)))
 
-	clf.fit(X, y)
-	print("Training score:", clf.score(X,y))
+			if len(sys.argv) > 1:
+				if len(sys.argv) > 2:
+					files = sys.argv[1:]
+				else : # Expand wildcard
+					files = glob.glob(sys.argv[1])
 
-	if len(sys.argv) > 1:
-		if len(sys.argv) > 2:
-			files = sys.argv[1:]
-		else : # Expand wildcard
-			files = glob.glob(sys.argv[1])
+				for i, fn in enumerate(files):
+					X_test = denominant_color(fn, show_img = None)
+					y_test = y2c[clf.predict(X_test.reshape(-1))[0]]
+					print("[Classifier] May {} in {}.".format(y_test, fn))
 
-		for i, fn in enumerate(files):
-			X_test = denominant_color(fn, cache=None, show_img = None)
-			y_test = y2c[clf.predict(X_test.reshape(-1))[0]]
-			print("May {} in {}.".format(y_test, fn))
-
-			if disp:
-				img = denominant_color(fn, cache=None, rtn_img = True,
-					show_img = 'overlay')
-				canvas = Image.new('RGBA', img.size)
-				painter = ImageDraw.Draw(canvas)
-				if os.path.isfile(FONT):
-					font = ImageFont.truetype(FONT, 72)
-				else:
-					font = ImageFont.load_default(72)
-				painter.text((CIR_MARGIN +2, CIR_RADIUS * 2 + CIR_MARGIN * 3 +2),
-					y_test, font=font,
-					fill = RGBA_SHADOW)
-				painter.text((CIR_MARGIN, CIR_RADIUS * 2 + CIR_MARGIN * 3),
-					y_test, font=font,
-					fill = tuple([int(c) for c in X_test[int(len(X_test)*2/3)]]))
-				Image.alpha_composite(img, canvas).show(title = fn)
+					if disp:
+						text = y_test + '/' + clfName
+						img = denominant_color(fn, rtn_img = True,
+							show_img = 'overlay')
+						canvas = Image.new('RGBA', img.size)
+						painter = ImageDraw.Draw(canvas)
+						if os.path.isfile(FONT):
+							font = ImageFont.truetype(FONT, 36)
+						else:
+							font = ImageFont.load_default(36)
+						painter.text((CIR_MARGIN +2, CIR_RADIUS * 2 + CIR_MARGIN * 3 +2),
+							text, font=font,
+							fill = RGBA_SHADOW)
+						painter.text((CIR_MARGIN, CIR_RADIUS * 2 + CIR_MARGIN * 3),
+							text, font=font,
+							fill = tuple([int(c) for c in X_test[int(len(X_test)*2/3)]]))
+						Image.alpha_composite(img, canvas).show(title = fn)
