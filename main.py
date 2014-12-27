b43f9df7aaf4a7f52e68119db45b09fcb3d1e46a
MidoriYakumo <macrobull.acg.0@gmail.com>
2014-12-28 02:04:37 +0800

    well

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Version	:0.0.1
 import cv2
 import numpy as np
 import os, sys, time, glob
-import h5py
+import h5py, pickle
 
 from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from sklearn.svm import SVC, NuSVC
@@ -28,7 +28,7 @@ __version__ = '0.0.1'
 SIZE_THUMB = 256
 SIZE_PREVIEW = 512
 
-K = 6
+K = 8
 ATTEMPTS = 10
 MAX_ITER = 10
 EPSILON = 1.0
@@ -40,7 +40,33 @@ CIR_CANVAS_SCALE = np.array(2)
 CIR_BORDER_LIGHT = (222, 222, 222)
 CIR_BORDER_DARK = (88, 88, 88)
 
+tuning = True
+comparison = True
 
+#imported_pylab = False
+fig = None
+
+def better_imshow(title, img, reuse = True):
+#	if title is None: title = "%sx%s" % img.shape[:2]
+	if np.any(np.frombuffer(title.encode(), dtype = np.uint8)>=0x80):
+		global fig
+		from pylab import figure, subplot, imshow
+		if not reuse or fig is None:
+			fig = figure(figsize = (img.shape[1]/96, (img.shape[0]+30)/96))
+#		plt = fig.add_subplot(111, title = title)
+#		plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+		subplot(111, title = title)
+		imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+	else:
+		cv2.imshow(title, img)
+		imageDisplayed()
+
+def imageDisplayed(delay = 1):
+	c = cv2.waitKey(delay)
+	if c>0:
+		print("[Key pressed]", c)
+		if c in [27, 113]: cv2.destroyAllWindows()
 
 def thumbnail(img, dMax, resample = cv2.INTER_LINEAR):
 
@@ -119,12 +145,6 @@ def draw_palette(img, colors):
 		cv2.circle(img, ((CIR_MARGIN + CIR_RADIUS)*(2*i+1), CIR_MARGIN + CIR_RADIUS) , CIR_RADIUS - 2, CIR_BORDER_LIGHT, 3, cv2.LINE_AA)
 		cv2.circle(img, ((CIR_MARGIN + CIR_RADIUS)*(2*i+1), CIR_MARGIN + CIR_RADIUS) , CIR_RADIUS, CIR_BORDER_DARK, 1, cv2.LINE_AA)
 
-def imageDisplayed(delay = 1):
-	c = cv2.waitKey(delay)
-	if c>0:
-		print("[Key pressed]", c)
-		if c in [27, 113]: cv2.destroyAllWindows()
-
 def color_extractor(fn, mode):
 	img = cv2.imread(fn)
 	thumb = thumbnail(img, SIZE_THUMB, cv2.INTER_LINEAR)
@@ -140,7 +160,8 @@ def color_extractor(fn, mode):
 
 	if preview is not None:
 		draw_palette(preview, colors)
-		cv2.imshow(str(fn.encode())+' damn no unicode support', preview)
+#		cv2.imshow(str(fn.encode())+' damn no unicode support', preview)
+		better_imshow(fn, preview, reuse = True)
 		imageDisplayed()
 
 	return colors
@@ -148,21 +169,7 @@ def color_extractor(fn, mode):
 def char_extractor(fn):
 	return [fn.split('.')[0]] # CHARACTER.X.jpg
 
-def append_chars(chars):
-	global y2c, c2y, cCnt, cCntr
-	if len(chars) > 1:
-		print("[Dataset] Only 1 character supported now.")
-		return 0
-	char = chars[0]
-	if char not in c2y:
-		y2c[cCntr] = char
-		c2y[char] = cCntr
-		cCnt[char] = 0
-		cCntr += 1
-	y.append(c2y[char])
-	cCnt[char] += 1
-
-def cache(key, miss_func = None, mode='r', fn = 'cache.hdf5'):
+def cache(key, miss_func = None, mode='r', fn = 'cache'+str(K)+'.hdf5', is_obj = False):
 	if not os.path.isfile(fn):
 		hdf = h5py.File(fn, "w")
 		hdf.close()
@@ -172,146 +179,161 @@ def cache(key, miss_func = None, mode='r', fn = 'cache.hdf5'):
 	if key in hdf:
 		print('[Cache hit] ' + key)
 		ret = np.array(hdf[key])
+		if is_obj:
+			ret = pickle.loads(ret.tobytes())
 	else:
 		ret = miss_func()
 		if type(ret) is not np.ndarray:
 			print('[Cache] ret of miss_func('+key+')is not an array')
 		if 'w' in mode:
-			hdf.create_dataset(key, data=ret)
+			if is_obj:
+				data = np.frombuffer(pickle.dumps(ret), dtype = np.uint8)
+			else:
+				data = ret
+			hdf.create_dataset(key, data=data)
 	hdf.close()
 	return ret
 
-def md5(filename):
-	return os.popen("md5sum '"+filename+"'").read().split()[0]
+class illust_ds():
+	def __init__(self):
+		self.X, self.y= [], []
+		self.c2y, self.y2c = {}, {}
+		self.cntr = 0
+		self.cnt = {}
+		self.size = 0
 
+	def append(self, features, characters):
+		if len(characters) > 1:
+			print("[Dataset] Only 1 character supported now.")
+			return 1
+		self.X.append(features)
+		char = characters[0]
+		if char not in self.c2y:
+			self.y2c[self.cntr] = char
+			self.c2y[char] = self.cntr
+			self.cnt[char] = 0
+			self.cntr += 1
+		self.y.append(self.c2y[char])
+		self.cnt[char] += 1
+		self.size += 1
 
-if __name__ == '__main__':
-	X, y= [], []
-	c2y, y2c = {}, {}
-	cCntr = 0
-	cCnt = {}
+	def __repr__(self):
+		return "illust_ds(size=%(size)s, cnt=%(cnt)s)" % self.__dict__
 
-	dsDir = 'img/'
+	def genIndices(self, cnt = 30):
+		self.X = np.array(self.X)
+		self.y = np.array(self.y)
+		indice = list(range(len(self.X)))
+		self.indices = []
+		for i in range(cnt):
+			np.random.shuffle(indice)
+			self.indices.append(indice.copy())
+
+def prepare_dataset(dsDir = 'img'):
+	dsDir += '/'
 	fileList = list(os.listdir(dsDir))
 	total = len(fileList)
 	print("[Info] Extracting features from {} files.".format(total))
+	dataset = illust_ds()
 	for i, fn in enumerate(fileList):
 #		try:
 			if fn.endswith('jpg') or fn.endswith('png'):
 				print('[Info] {:4}/{:4}:{}'.format(i, total, fn))
-				colors = cache(md5(dsDir+fn), lambda :color_extractor(dsDir + fn, 'palette').reshape(-1), 'w')
-				X.append(colors)
-				append_chars(char_extractor(fn))
+				colors = cache('raw/'+md5(dsDir+fn),
+					lambda :color_extractor(dsDir + fn, 'palette').reshape(-1),
+					'w')
+				dataset.append(colors, char_extractor(fn))
 #		except Exception as e:
 #			print(fn, e)
 
-	print("[Classifier] Dataset y={}, count = {}".format(c2y, cCnt))
+	return dataset
 
-	imageDisplayed(0)
 
-#
-#if __name__ == '__main__':
-#	X = np.array(X)
-#	y = np.array(y)
-#	r_c = list(range(len(X)))
-#	r_cs = []
-#	for i in range(30):
-#		np.random.shuffle(r_c)
-#		r_cs.append(r_c.copy())
-#
-##	tuning = False
-#	if tuning:
-#
-#		sMax = 0
-##		for p in np.linspace(-20.0, 20.0, 15):
-##		for p in np.linspace(1e-4, 2.0, 15):
-#		for p in range(4,6):
-#			param = dict(kernel = 'poly', degree = p, gamma = 1.8)
-#
-#			clf = SVC(**param)
-#
-#			s = 0
-#			for r_c in r_cs:
-#				X_c, y_c = X[r_c], y[r_c]
-#				sCnt = int(0.9 * len(X))
-#				clf.fit(X_c[:sCnt], y_c[:sCnt])
-#				s += clf.score(X_c[sCnt:],y_c[sCnt:])
-#
-#			s /= len(r_cs)
-#			print("[Classifier] Cross_valid score = {:2},\t param = {}".format(s, param))
-#
-#			if s>sMax: clfBest, sMax = clf, s
-#
-#		print("[Classifier] ", clfBest.get_params())
-#		classifiers = [clfBest]
-#
-#	else:
-#		classifiers = [
-#			SVC(kernel = 'poly', degree = 4, gamma = 1.8), # fair
-##			NuSVC(kernel = 'linear', nu = p, degree = 5), #suck
-#			MultinomialNB(), # fair
-#			GaussianNB(), # fair
-##			BernoulliNB(), # suck
-#			LDA(), # good
-##			QDA(), # bug
-#			AdaBoostClassifier(), # bad
-#			GradientBoostingClassifier(n_estimators = 31), # fair+ slow
-#			BaggingClassifier(n_estimators = 45), # fair+
-#			DecisionTreeClassifier(), # bad
-##			ExtraTreeClassifier(), # bad
-#			RandomForestClassifier(), # bad
-##			RadiusNeighborsClassifier(), # suck eee
-#			LabelPropagation(kernel = 'knn'), #suck
-##			LabelSpreading(kernel = 'knn'), #suck
-#			KNeighborsClassifier(n_neighbors = 25, weights = 'distance') # fair+
-#			]
-##		comparison = False
-#		if not comparison:
-#			classifiers = classifiers[:1]
-#	for clf in classifiers:
-#		clfName = str(clf).split('(')[0]
-#		s = 0
-#		for r_c in r_cs:
-#			X_c, y_c = X[r_c], y[r_c]
-#			sCnt = int(0.9 * len(X))
-#			clf.fit(X_c[:sCnt], y_c[:sCnt])
-#			s += clf.score(X_c[sCnt:],y_c[sCnt:])
-#
-#		s /= len(r_cs)
-#		print("[Classifier] {} cross_valid score:{:3}".format(clfName, s))
-#
-#		clf.fit(X, y)
-#		print("[Classifier] {} Training score:{}".format(clfName, clf.score(X,y)))
-#
-#		if len(sys.argv) > 1:
-#			if len(sys.argv) > 2:
-#				files = sys.argv[1:]
-#			else : # Expand wildcard
-#				files = glob.glob(sys.argv[1])
-#
-#			for i, fn in enumerate(files):
-#				X_test = denominant_color(fn, show_img = None)
-#				y_test = y2c[clf.predict(X_test.reshape(-1))[0]]
-#				print("[Classifier] May {} in {}.".format(y_test, fn))
-#
-#				if disp:
-#					text = y_test + '/' + clfName
-#					img = denominant_color(fn, rtn_img = True,
-#						show_img = 'overlay')
-#					canvas = Image.new('RGBA', img.size)
-#					painter = ImageDraw.Draw(canvas)
-#					if os.path.isfile(FONT):
-#						font = ImageFont.truetype(FONT, 36)
-#					else:
-#						font = ImageFont.load_default(36)
-#					painter.text((CIR_MARGIN +2, CIR_RADIUS * 2 + CIR_MARGIN * 3 +2),
-#						text, font=font,
-#						fill = RGBA_SHADOW)
-#					painter.text((CIR_MARGIN, CIR_RADIUS * 2 + CIR_MARGIN * 3),
-#						text, font=font,
-#						fill = tuple([int(c) for c in X_test[int(len(X_test)*2/3)]]))
-#					Image.alpha_composite(img, canvas).show(title = fn)
-#
-#
-#
-#
+def md5(filename):
+	return os.popen("md5sum '"+filename+"'").read().split()[0]
+
+if __name__ == '__main__':
+	dataset = cache('/classifier/dataset/img', lambda :prepare_dataset(), 'w', is_obj=True)
+	print("[Classifier] Dataset y={}, count = {}".format(dataset.c2y, dataset.cnt))
+	dataset.genIndices()
+
+	tuning = False
+	if tuning:
+		sMax = 0
+#		for p in np.linspace(-20.0, 20.0, 15):
+#		for p in np.linspace(1e-4, 2.0, 15):
+		for p in range(4,6):
+			param = dict(kernel = 'poly', degree = p, gamma = 1.8)
+			clf = SVC(**param)
+
+			s = 0
+			for r in dataset.indices:
+				X_c, y_c = dataset.X[r], dataset.y[r]
+				sCnt = int(0.9 * dataset.size)
+				clf.fit(X_c[:sCnt], y_c[:sCnt])
+				s += clf.score(X_c[sCnt:],y_c[sCnt:])
+			s /= len(dataset.indices)
+			print("[Classifier] Cross_valid score = {:2},\t param = {}".format(s, param))
+			if s>sMax: clfBest, sMax = clf, s
+		print("[Classifier] ", clfBest.get_params())
+		classifiers = [clfBest]
+	else:
+		classifiers = [
+			KNeighborsClassifier(n_neighbors = 25, weights = 'distance'), # fair+
+			LDA(), # good
+			SVC(kernel = 'poly', degree = 4, gamma = 1.8), # fair
+#			NuSVC(kernel = 'linear', nu = p, degree = 5), #suck
+			MultinomialNB(), # fair
+			GaussianNB(), # fair
+#			BernoulliNB(), # suck
+#			QDA(), # bug
+			AdaBoostClassifier(), # bad
+			BaggingClassifier(n_estimators = 45), # fair+
+			DecisionTreeClassifier(), # bad
+#			ExtraTreeClassifier(), # bad
+			RandomForestClassifier(), # bad
+#			RadiusNeighborsClassifier(), # suck eee
+			LabelPropagation(kernel = 'knn'), #suck
+#			LabelSpreading(kernel = 'knn'), #suck
+			GradientBoostingClassifier(n_estimators = 31) # fair+ slow
+			]
+		comparison = False
+		if not comparison:
+			classifiers = classifiers[:1]
+	for clf in classifiers:
+		clfName = str(clf).split('(')[0]
+		s = 0
+		for r_c in dataset.indices:
+			X_c, y_c = dataset.X[r_c], dataset.y[r_c]
+			sCnt = int(0.9 * dataset.size)
+			clf.fit(X_c[:sCnt], y_c[:sCnt])
+			s += clf.score(X_c[sCnt:],y_c[sCnt:])
+
+		s /= len(dataset.indices)
+		print("[Classifier] {} cross_valid score:{:3}".format(clfName, s))
+
+		clf.fit(dataset.X, dataset.y)
+		print("[Classifier] {} Training score:{}".format(clfName, clf.score(dataset.X, dataset.y)))
+
+		if len(sys.argv) > 1:
+			if len(sys.argv) > 2:
+				files = sys.argv[1:]
+			else : # Expand wildcard
+				files = glob.glob(sys.argv[1])
+
+			for i, fn in enumerate(files):
+				img = cv2.imread(fn)
+				thumb = thumbnail(img, SIZE_THUMB, cv2.INTER_LINEAR)
+
+				body = focus(thumb)
+				colors = color_quantization(body)
+				y_test = clf.predict(colors.reshape(-1))[0]
+				char = dataset.y2c[y_test]
+				print("[Classifier] May {} in {}.".format(char, fn))
+
+				preview = thumbnail(img, SIZE_PREVIEW)
+				preview = focus(preview, rtn_img = True)
+				draw_palette(preview, colors)
+#				better_imshow(clfName+':'+char, preview)
+				better_imshow(clfName+':'+char, preview, reuse = False)
+				imageDisplayed()
